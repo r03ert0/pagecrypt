cd3500fc6304c2f1fb1e018104faeaac737e45bc
r03ert0 <robertotoro@gmail.com>
2021-11-02 10:06:25 +0100

    allow custom password prompt templates

--- src/core.ts
+++ src/core.ts
@@ -66,6 +66,26 @@ export async function encryptHTML(inputHTML: string, password: string) {
 }
 
 /**
+ * Encrypt an HTML string with a given password and a custom prompt template.
+ * The resulting page can be viewed and decrypted by opening the output HTML
+ * file in a browser, and entering the correct password.
+ *
+ * @param {string} inputHTML The HTML string to encrypt.
+ * @param {string} password The password which will be used to encrypt + decrypt the content.
+ * @param {string} promptTemplate A custom prompt template based on decrypt-template.html
+ * @returns A promise that will resolve with the encrypted HTML content
+ */
+ export async function encryptHTMLCustom(inputHTML: string, password: string, promptTemplate: string) {
+    return (promptTemplate as string).replace(
+        /<!--ENCRYPTED PAYLOAD-->/,
+        `<pre class="hidden">${await getEncryptedPayload(
+            inputHTML,
+            password,
+        )}</pre>`,
+    )
+}
+
+/**
  * Generate a random password of a given length.
  *
  * @param {number} length The password length.
